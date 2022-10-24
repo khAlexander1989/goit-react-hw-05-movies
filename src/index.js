3bd0d4ea10eb1cf71f31b0e1543a247d8ebabe8f
Alexander Khardin <khporter1984@gmail.com>
2022-10-24 14:58:48 +0300

    add basename to browser router

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-05-movies">
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-    ;
   </React.StrictMode>
 );
